f4bad68d60143f52915a7cd6d84d2c5d45cab9c8
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-11-29 14:39:23 +0530

    Added method for create,update,delete,get,getall in categoryService

---
+++ src/main/java/com/lcwd/electronic/store/electronicstore/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.lcwd.electronic.store.electronicstore.service;
+
+import com.lcwd.electronic.store.electronicstore.dtos.CategoryDto;
+import com.lcwd.electronic.store.electronicstore.dtos.PageableResponse;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    CategoryDto createCategory(CategoryDto categoryDto);
+
+    CategoryDto updateCategory(CategoryDto categoryDto,String categoryId);
+
+    CategoryDto getCategoryById(String categoryId);
+
+    PageableResponse<CategoryDto> getAllCategory();
+
+    void deleteCategory(String categoryId);
+
+
+}
