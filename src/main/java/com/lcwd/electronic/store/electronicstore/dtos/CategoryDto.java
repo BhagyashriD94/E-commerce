7d2c783f496b21306c243d9083d3b945fd43582c
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-11-29 14:06:48 +0530

    creating a CategoryDto

---
+++ src/main/java/com/lcwd/electronic/store/electronicstore/dtos/CategoryDto.java
@@ -0,0 +1,28 @@
+package com.lcwd.electronic.store.electronicstore.dtos;
+
+import com.lcwd.electronic.store.electronicstore.validation.ImageNameValid;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryDto {
+
+    private String categoryId;
+    @NotBlank(message="write something in title")
+    @Size(max=10,message = "title containing maximum 10 character")
+    private String title;
+    @NotBlank
+    @Size(max=1000,message = "write a description in max 1000 charater")
+    private String description;
+    @ImageNameValid
+    private String coverImage;
+
+
+}
