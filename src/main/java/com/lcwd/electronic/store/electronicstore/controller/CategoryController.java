8e1754f72f73daddf4aa812917213772ff779af2
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-11-29 15:34:51 +0530

    Added controller level logic for create Category

---
+++ src/main/java/com/lcwd/electronic/store/electronicstore/controller/CategoryController.java
@@ -0,0 +1,31 @@
+package com.lcwd.electronic.store.electronicstore.controller;
+
+import com.lcwd.electronic.store.electronicstore.dtos.CategoryDto;
+import com.lcwd.electronic.store.electronicstore.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+    @PostMapping("/category")
+    public ResponseEntity<CategoryDto> createCategory(@Valid @RequestBody CategoryDto categoryDto){
+        CategoryDto categoryDto1 = this.categoryService.createCategory(categoryDto);
+     return new ResponseEntity<>(categoryDto1, HttpStatus.CREATED) ;
+    }
+
+//    public ResponseEntity<CategoryDto> updateCategory(@Valid @RequestBody CategoryDto categoryDto, @PathVariable String categoryId){
+//        CategoryDto categoryDto1 = this.categoryService.updateCategory(categoryDto, categoryId);
+//        return new ResponseEntity<>(categoryDto1,HttpStatus.OK);
+//    }
+//
+
+
+
+}
