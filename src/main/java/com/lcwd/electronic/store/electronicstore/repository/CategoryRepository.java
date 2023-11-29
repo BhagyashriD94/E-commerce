b72903132e917a520663b6fcdd39c1f1c1c4911e
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-11-29 14:37:11 +0530

    creating a repository for Category

---
+++ src/main/java/com/lcwd/electronic/store/electronicstore/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.lcwd.electronic.store.electronicstore.repository;
+
+import com.lcwd.electronic.store.electronicstore.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,String> {
+}
