808ffb3854a291b5278b108d6b531ffb092fd9d5
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-11-29 15:20:09 +0530

    Added a service level logic for get all category,get category and delete category

--- src/main/java/com/lcwd/electronic/store/electronicstore/service/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/lcwd/electronic/store/electronicstore/service/serviceImpl/CategoryServiceImpl.java
@@ -5,13 +5,19 @@ import com.lcwd.electronic.store.electronicstore.dtos.CategoryDto;
 import com.lcwd.electronic.store.electronicstore.dtos.PageableResponse;
 import com.lcwd.electronic.store.electronicstore.entity.Category;
 import com.lcwd.electronic.store.electronicstore.exception.ResourceNotFoundException;
+import com.lcwd.electronic.store.electronicstore.helper.Helper;
 import com.lcwd.electronic.store.electronicstore.repository.CategoryRepository;
 import com.lcwd.electronic.store.electronicstore.service.CategoryService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -42,17 +48,24 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryDto getCategoryById(String categoryId) {
-        return null;
+        Category category = this.categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException(AppConstant.Not_Found));
+        return this.modelMapper.map(category,CategoryDto.class);
     }
 
     @Override
-    public PageableResponse<CategoryDto> getAllCategory() {
-        return null;
+    public PageableResponse<CategoryDto> getAllCategory(int pageNumber, int pageSize, String sortBy, String sortDir) {
+       Sort sort=(sortDir.equalsIgnoreCase("desc"))?(Sort.by(sortBy).descending()):(Sort.by(sortBy).ascending());
+        Pageable p= PageRequest.of(pageNumber,pageSize,sort);
+        Page<Category> page = this.categoryRepository.findAll(p);
+        PageableResponse<CategoryDto> pageableResponse = Helper.getPageableResponse(page, CategoryDto.class);
+        return pageableResponse;
     }
 
 
     @Override
     public void deleteCategory(String categoryId) {
+        Category category = this.categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException(AppConstant.Not_Found));
+        this.categoryRepository.delete(category);
 
     }
 }
