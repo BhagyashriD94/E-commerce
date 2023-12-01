2b115427db3d47b8e3c5b0a48b34fd97b3086ba5
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-12-01 06:08:37 +0530

    Added logic for get resource

--- src/main/java/com/lcwd/electronic/store/electronicstore/service/serviceImpl/FileServiceImpl.java
+++ src/main/java/com/lcwd/electronic/store/electronicstore/service/serviceImpl/FileServiceImpl.java
@@ -11,31 +11,33 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.UUID;
+
 @Service
 public class FileServiceImpl implements FileService {
 
-    Logger logger= LoggerFactory.getLogger(FileServiceImpl.class);
+    Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
+
     @Override
     public String uploadFile(MultipartFile file, String path) throws IOException {
 
-        String originalFilename=file.getOriginalFilename();
-         logger.info("Filename: {}",originalFilename);
+        String originalFilename = file.getOriginalFilename();
+        logger.info("Filename: {}", originalFilename);
         String filename = UUID.randomUUID().toString();
-        String extension =originalFilename.substring(originalFilename.lastIndexOf("."));
-        String filenamewithextention=filename+extension;
-        String fullpathwithfilename=path+File.separator+filenamewithextention;
-        logger.info("full image path: {}",fullpathwithfilename);
-        if(extension.equalsIgnoreCase(".png") || extension.equalsIgnoreCase(".jpg") || extension.equalsIgnoreCase(".jpeg")) {
-          logger.info("file extension is {}",extension);
-            File folder=new File(path);
-            if(!folder.exists()){
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String filenamewithextention = filename + extension;
+        String fullpathwithfilename = path + File.separator + filenamewithextention;
+        logger.info("full image path: {}", fullpathwithfilename);
+        if (extension.equalsIgnoreCase(".png") || extension.equalsIgnoreCase(".jpg") || extension.equalsIgnoreCase(".jpeg")) {
+            logger.info("file extension is {}", extension);
+            File folder = new File(path);
+            if (!folder.exists()) {
 
                 folder.mkdirs();
             }
             Files.copy(file.getInputStream(), Paths.get(fullpathwithfilename));
-             return filenamewithextention;
-        }else{
-            throw new BadApiRequest("File with this"+extension+"not allow");
+            return filenamewithextention;
+        } else {
+            throw new BadApiRequest("File with this" + extension + "not allow");
         }
 
 
@@ -43,8 +45,8 @@ public class FileServiceImpl implements FileService {
 
     @Override
     public InputStream getResource(String path, String name) throws FileNotFoundException {
-        String fullpath= path+File.separator+name;
-        InputStream inputstream=new FileInputStream(fullpath);
+        String fullpath = path + File.separator + name;
+        InputStream inputstream = new FileInputStream(fullpath);
         return inputstream;
     }
 }
