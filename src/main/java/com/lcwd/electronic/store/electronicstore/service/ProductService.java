82f3a8eb4ef1a4809bd9b58ba87441e7ebba3512
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-12-04 16:55:24 +0530

    Added create, update,get,getall,delete method and logic at service layer

---
+++ src/main/java/com/lcwd/electronic/store/electronicstore/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.lcwd.electronic.store.electronicstore.service;
+
+import com.lcwd.electronic.store.electronicstore.dtos.PageableResponse;
+import com.lcwd.electronic.store.electronicstore.dtos.ProductDto;
+
+import java.util.List;
+
+public interface ProductService {
+
+    ProductDto createProduct(ProductDto productDto);
+
+    ProductDto updateProduct(ProductDto productDto,String productId);
+
+    ProductDto getProductById(String productId);
+
+    PageableResponse<ProductDto> getAllProduct(int pageNumber, int pageSize, String sortBy, String sortDir);
+
+    void deleteProduct(String productId);
+
+    PageableResponse<ProductDto> getAllLive(int pageNumber, int pageSize, String sortBy, String sortDir);
+    PageableResponse<ProductDto> searchByTitle(String subTitle,int pageNumber, int pageSize, String sortBy, String sortDir);
+
+
+
+}
---
+++ src/main/java/com/lcwd/electronic/store/electronicstore/service/serviceImpl/ProductServiceImpl.java
@@ -0,0 +1,81 @@
+package com.lcwd.electronic.store.electronicstore.service.serviceImpl;
+
+import com.lcwd.electronic.store.electronicstore.constants.AppConstant;
+import com.lcwd.electronic.store.electronicstore.dtos.PageableResponse;
+import com.lcwd.electronic.store.electronicstore.dtos.ProductDto;
+import com.lcwd.electronic.store.electronicstore.entity.Product;
+import com.lcwd.electronic.store.electronicstore.exception.ResourceNotFoundException;
+import com.lcwd.electronic.store.electronicstore.helper.Helper;
+import com.lcwd.electronic.store.electronicstore.repository.ProductRepository;
+import com.lcwd.electronic.store.electronicstore.service.ProductService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+    @Override
+    public ProductDto createProduct(ProductDto productDto) {
+        String stringId = UUID.randomUUID().toString();
+        productDto.setProductId(stringId);
+        Product product = this.modelMapper.map(productDto, Product.class);
+        Product product1 = this.productRepository.save(product);
+        return this.modelMapper.map(product1,ProductDto.class);
+    }
+
+    @Override
+    public ProductDto updateProduct(ProductDto productDto, String productId) {
+        Product product = this.productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException(AppConstant.Not_Found));
+        product.setTitle(productDto.getTitle());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        product.setAddedDate(productDto.getAddedDate());
+        product.setQuantity(productDto.getQuantity());
+        Product updatedproduct = this.productRepository.save(product);
+        return this.modelMapper.map(updatedproduct,ProductDto.class);
+    }
+
+    @Override
+    public ProductDto getProductById(String productId) {
+        Product product = this.productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException(AppConstant.Not_Found));
+        return this.modelMapper.map(product,ProductDto.class);
+    }
+
+    @Override
+    public PageableResponse<ProductDto> getAllProduct(int pageNumber, int pageSize, String sortBy, String sortDir) {
+        Sort sort=(sortBy.equalsIgnoreCase("desc"))?(Sort.by(sortBy).descending()):(Sort.by(sortBy).ascending());
+        Pageable p= PageRequest.of(pageNumber,pageSize,sort);
+        Page<Product> page = this.productRepository.findAll(p);
+        PageableResponse<ProductDto> pageableResponse = Helper.getPageableResponse(page, ProductDto.class);
+        return pageableResponse;
+    }
+
+    @Override
+    public void deleteProduct(String productId) {
+        Product product = this.productRepository.findById(productId).orElseThrow(() -> new ResourceNotFoundException(AppConstant.Not_Found));
+         this.productRepository.delete(product);
+    }
+
+    @Override
+    public PageableResponse<ProductDto> getAllLive(int pageNumber, int pageSize, String sortBy, String sortDir) {
+        return null;
+    }
+
+    @Override
+    public PageableResponse<ProductDto> searchByTitle(String subTitle,int pageNumber, int pageSize, String sortBy, String sortDir) {
+        return null;
+    }
+}
