6579e4b534955778f634b7d92f242c17c71c5452
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-12-08 03:43:16 +0530

    Refactor,List of product into page

--- src/main/java/com/lcwd/electronic/store/electronicstore/repository/ProductRepository.java
+++ src/main/java/com/lcwd/electronic/store/electronicstore/repository/ProductRepository.java
@@ -1,12 +1,15 @@
 package com.lcwd.electronic.store.electronicstore.repository;
 
 import com.lcwd.electronic.store.electronicstore.entity.Product;
+import org.springframework.data.domain.Page;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product,String> {
 
-    List<Product> findByTitleContaining(String subTitle);
-    List<Product> findByLiveTrue();
+    Page<Product> findByTitleContaining(String subTitle, Pageable p);
+    Page<Product> findByLiveTrue(Pageable p);
 }
