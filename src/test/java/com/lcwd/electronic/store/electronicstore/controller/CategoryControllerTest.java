ef9e6272c39584a6e9d7ecddeff4650120642761
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-12-13 10:50:30 +0530

    Added test case for get all categories in CategoryControllerTest class

--- src/test/java/com/lcwd/electronic/store/electronicstore/controller/CategoryControllerTest.java
+++ src/test/java/com/lcwd/electronic/store/electronicstore/controller/CategoryControllerTest.java
@@ -2,6 +2,7 @@ package com.lcwd.electronic.store.electronicstore.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lcwd.electronic.store.electronicstore.dtos.CategoryDto;
+import com.lcwd.electronic.store.electronicstore.dtos.PageableResponse;
 import com.lcwd.electronic.store.electronicstore.dtos.UserDto;
 import com.lcwd.electronic.store.electronicstore.entity.Category;
 import com.lcwd.electronic.store.electronicstore.service.CategoryService;
@@ -17,6 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -39,24 +41,14 @@ public class CategoryControllerTest {
 
     @BeforeEach
     public void init() {
-        category = Category.builder()
-                .categoryId(UUID.randomUUID().toString())
-                .title("mobile")
-                .description("this category contain mobile related product")
-                .coverImage("mob.png").build();
+        category = Category.builder().categoryId(UUID.randomUUID().toString()).title("mobile").description("this category contain mobile related product").coverImage("mob.png").build();
     }
 
     @Test
     public void createCategoryTest() throws Exception {
         CategoryDto categoryDto = modelMapper.map(category, CategoryDto.class);
         Mockito.when(categoryService.createCategory(Mockito.any())).thenReturn(categoryDto);
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/category")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(convertObjectToJsonString(category))
-                        .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.title").exists());
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/category").contentType(MediaType.APPLICATION_JSON).content(convertObjectToJsonString(category)).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isCreated()).andExpect(jsonPath("$.title").exists());
     }
 
     private String convertObjectToJsonString(Object category) throws Exception {
@@ -65,20 +57,30 @@ public class CategoryControllerTest {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
-        }}
-
-        @Test
-        public void updateCategoryTest () throws Exception {
-            String categoryId = category.getCategoryId();
-            CategoryDto categoryDto = modelMapper.map(category, CategoryDto.class);
-            Mockito.when(categoryService.updateCategory(Mockito.any(), Mockito.anyString())).thenReturn(categoryDto);
-            this.mockMvc.perform(MockMvcRequestBuilders.put("/api/category/" + categoryId)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(convertObjectToJsonString(category))
-                            .accept(MediaType.APPLICATION_JSON))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.title").exists());
         }
     }
+
+    @Test
+    public void updateCategoryTest() throws Exception {
+        String categoryId = category.getCategoryId();
+        CategoryDto categoryDto = modelMapper.map(category, CategoryDto.class);
+        Mockito.when(categoryService.updateCategory(Mockito.any(), Mockito.anyString())).thenReturn(categoryDto);
+        this.mockMvc.perform(MockMvcRequestBuilders.put("/api/category/" + categoryId).contentType(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(convertObjectToJsonString(category)).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.title").exists());
+    }
+
+    @Test
+    public void getAllCategoryTest() throws Exception {
+        CategoryDto dto1 = CategoryDto.builder().title("Tv").description("this is anroid Tv of Haier company").coverImage("tv.png").build();
+        CategoryDto dto2 = CategoryDto.builder().title("Refrigerator").description("this is anroid Tv of Haier company").coverImage("tv.png").build();
+        CategoryDto dto3 = CategoryDto.builder().title("Cooler").description("this is anroid Tv of Haier company").coverImage("tv.png").build();
+        CategoryDto dto4 = CategoryDto.builder().title("mobilephone").description("this is anroid Tv of Haier company").coverImage("tv.png").build();
+        PageableResponse<CategoryDto> pageableResponse = new PageableResponse<>();
+        pageableResponse.setContent(Arrays.asList(dto1, dto2, dto3, dto4));
+        pageableResponse.setLastpage(false);
+        pageableResponse.setPageSize(10);
+        pageableResponse.setPageNumber(100);
+        pageableResponse.setTotalElements(1000l);
+        Mockito.when(categoryService.getAllCategory(Mockito.anyInt(), Mockito.anyInt(), Mockito.anyString(), Mockito.anyString())).thenReturn(pageableResponse);
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/categories").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk());
+    }
+}
