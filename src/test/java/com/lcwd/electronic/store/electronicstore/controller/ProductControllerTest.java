7f6331b620311b98d9106e9521ee37a3ef932668
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-12-13 11:13:53 +0530

    Added test case for create product in ProductControllerTest class

---
+++ src/test/java/com/lcwd/electronic/store/electronicstore/controller/ProductControllerTest.java
@@ -0,0 +1,74 @@
+package com.lcwd.electronic.store.electronicstore.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lcwd.electronic.store.electronicstore.dtos.ProductDto;
+import com.lcwd.electronic.store.electronicstore.dtos.UserDto;
+import com.lcwd.electronic.store.electronicstore.entity.Product;
+import com.lcwd.electronic.store.electronicstore.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+    @MockBean
+    private ProductService productService;
+    @Autowired
+    private ProductController productController;
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Product product;
+
+    @BeforeEach
+    public void init() {
+        product = Product.builder()
+                .productId(UUID.randomUUID().toString())
+                .title("iphone")
+                .description("this phone havin good camera and 5G features")
+                .price(70000.00)
+                .quantity(2)
+                .descountprice(10000.00)
+                .stock(true)
+                .live(true)
+                .productImage("ipn.png").build();
+    }
+    @Test
+    public void createProductTest() throws Exception {
+        ProductDto productDto = modelMapper.map(product, ProductDto.class);
+        Mockito.when(productService.createProduct(Mockito.any())).thenReturn(productDto);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(convertObjectToJsonString(product))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").exists());
+    }
+    private String convertObjectToJsonString(Object product){
+        try{
+            return new ObjectMapper().writeValueAsString(product);
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
