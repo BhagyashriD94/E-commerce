5d24198262a2721816739e6ca37c81a1a6f8a038
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-12-29 05:31:14 +0530

    create Api for getOrderByUser

--- src/main/java/com/lcwd/electronic/store/electronicstore/controller/OrderController.java
+++ src/main/java/com/lcwd/electronic/store/electronicstore/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.lcwd.electronic.store.electronicstore.controller;
 
+import com.lcwd.electronic.store.electronicstore.constants.AppConstant;
 import com.lcwd.electronic.store.electronicstore.dtos.CreateOrderRequest;
 import com.lcwd.electronic.store.electronicstore.dtos.OrderDto;
 import com.lcwd.electronic.store.electronicstore.dtos.PageableResponse;
@@ -28,9 +29,18 @@ public class OrderController {
         ApiResponse apiResponse = ApiResponse.builder().message("order remove sucessfully").success(true).status(HttpStatus.OK).build();
         return new ResponseEntity<>(apiResponse,HttpStatus.OK);
     }
-//    @GetMapping("/order/{userId}")
-//    public ResponseEntity<List<OrderDto>> getOrderByUser(@PathVariable String userId){
-//        List<OrderDto> orderOfUser = orderService.getOrderOfUser(userId);
-//        return new ResponseEntity<>(orderOfUser,HttpStatus.OK);
+    @GetMapping("/order/{userId}")
+    public ResponseEntity<List<OrderDto>> getOrderByUser(@PathVariable String userId){
+        List<OrderDto> orderOfUser = orderService.getOrderOfUser(userId);
+        return new ResponseEntity<>(orderOfUser,HttpStatus.OK);
+    }
+//    @GetMapping("/orders")
+//    public ResponseEntity<PageableResponse<OrderDto>> getOrderByUser(@RequestParam(value="pageNumber",defaultValue = AppConstant.PAGE_NUMBER,required = false)int pageNumber,
+//                                                                     @RequestParam(value ="pageSize",defaultValue = AppConstant.PAGE_SIZE,required = false)int pageSize,
+//                                                                     @RequestParam(value ="sortBy",defaultValue = AppConstant.SORT_BY,required = false)String sortBy,
+//                                                                     @RequestParam(value ="sortDir",defaultValue = AppConstant.SORT_DIR,required = false)String sortDir)
+//    {
+//        PageableResponse<OrderDto> orders = orderService.getOrders(pageNumber, pageSize, sortBy, sortDir);
+//        return new ResponseEntity<>(orders,HttpStatus.OK);
 //    }
 }
