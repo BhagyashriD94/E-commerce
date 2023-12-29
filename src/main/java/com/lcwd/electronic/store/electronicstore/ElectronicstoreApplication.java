20abe97d95bf6ca737c09fa7e1aa35bbac9b9fb0
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2023-12-29 15:56:22 +0530

    Added implementation by CommandLineRunner

--- src/main/java/com/lcwd/electronic/store/electronicstore/ElectronicstoreApplication.java
+++ src/main/java/com/lcwd/electronic/store/electronicstore/ElectronicstoreApplication.java
@@ -1,15 +1,25 @@
 package com.lcwd.electronic.store.electronicstore;
 
 import com.lcwd.electronic.store.electronicstore.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class ElectronicstoreApplication {
+public class ElectronicstoreApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ElectronicstoreApplication.class, args);
-//	User u=new User();
-//	u.setUserId();
+
+    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println(passwordEncoder.encode("pr234"));
+
     }
 }
