8f8c1b0439effb501250c3a68b6c7d97742c7b06
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2024-01-06 16:41:32 +0530

    Added comments into OrderController

--- src/main/java/com/lcwd/electronic/store/electronicstore/controller/OrderController.java
+++ src/main/java/com/lcwd/electronic/store/electronicstore/controller/OrderController.java
@@ -18,29 +18,57 @@ import java.util.List;
 public class OrderController {
     @Autowired
     private OrderService orderService;
+
+    /**
+     * @auther Bhagyashri
+     * @param request
+     * @apiNote create api to save order data into database
+     * @return
+     */
     @PostMapping("/order")
-    public ResponseEntity<OrderDto> createOrder(@RequestBody CreateOrderRequest request){
+    public ResponseEntity<OrderDto> createOrder(@RequestBody CreateOrderRequest request) {
         OrderDto order = orderService.createOrder(request);
         return new ResponseEntity<>(order, HttpStatus.CREATED);
     }
+    /**
+     * @auther Bhagyashri
+     * @param orderId
+     * @apiNote create api to order data into database
+     * @return
+     */
     @DeleteMapping("/order/{orderId}")
-    public ResponseEntity<ApiResponse> removeorder(@PathVariable String orderId){
+    public ResponseEntity<ApiResponse> removeorder(@PathVariable String orderId) {
         orderService.removeOrder(orderId);
         ApiResponse apiResponse = ApiResponse.builder().message("order remove sucessfully").success(true).status(HttpStatus.OK).build();
-        return new ResponseEntity<>(apiResponse,HttpStatus.OK);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
+
+    /**
+     * @auther Bhagyashri
+     * @param userId
+     * @apiNote create api to retrived order data into database into userId
+     * @return
+     */
     @GetMapping("/order/{userId}")
-    public ResponseEntity<List<OrderDto>> getOrderByUser(@PathVariable String userId){
+    public ResponseEntity<List<OrderDto>> getOrderByUser(@PathVariable String userId) {
         List<OrderDto> orderOfUser = orderService.getOrderOfUser(userId);
-        return new ResponseEntity<>(orderOfUser,HttpStatus.OK);
+        return new ResponseEntity<>(orderOfUser, HttpStatus.OK);
     }
+    /**
+     * @auther Bhagyashri
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param sortDir
+     * @apiNote create api to retrived all order data from database
+     * @return
+     */
     @GetMapping("/orders")
-    public ResponseEntity<PageableResponse<OrderDto>> getOrderByUser(@RequestParam(value="pageNumber",defaultValue = AppConstant.PAGE_NUMBER,required = false)int pageNumber,
-                                                                     @RequestParam(value ="pageSize",defaultValue = AppConstant.PAGE_SIZE,required = false)int pageSize,
-                                                                     @RequestParam(value ="sortBy",defaultValue = AppConstant.SORT_BY,required = false)String sortBy,
-                                                                     @RequestParam(value ="sortDir",defaultValue = AppConstant.SORT_DIR,required = false)String sortDir)
-    {
+    public ResponseEntity<PageableResponse<OrderDto>> getOrderByUser(@RequestParam(value = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER, required = false) int pageNumber,
+                                                                     @RequestParam(value = "pageSize", defaultValue = AppConstant.PAGE_SIZE, required = false) int pageSize,
+                                                                     @RequestParam(value = "sortBy", defaultValue = AppConstant.SORT_BY, required = false) String sortBy,
+                                                                     @RequestParam(value = "sortDir", defaultValue = AppConstant.SORT_DIR, required = false) String sortDir) {
         PageableResponse<OrderDto> orders = orderService.getOrders(pageNumber, pageSize, sortBy, sortDir);
-        return new ResponseEntity<>(orders,HttpStatus.OK);
+        return new ResponseEntity<>(orders, HttpStatus.OK);
     }
 }
