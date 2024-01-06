c07e549580f894b7447741ff1a9de9bd6583d5ae
BhagyashriD94 <bhagyashrideshmane@gmail.com>
2024-01-06 16:25:39 +0530

    Added logger to CartController

--- src/main/java/com/lcwd/electronic/store/electronicstore/controller/CartController.java
+++ src/main/java/com/lcwd/electronic/store/electronicstore/controller/CartController.java
@@ -4,6 +4,8 @@ import com.lcwd.electronic.store.electronicstore.dtos.AddItemsToCartRequest;
 import com.lcwd.electronic.store.electronicstore.dtos.CartDto;
 import com.lcwd.electronic.store.electronicstore.helper.ApiResponse;
 import com.lcwd.electronic.store.electronicstore.service.CartService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    Logger logger= LoggerFactory.getLogger(CartController.class);
 
     /**
      * @auther Bhagyashri
@@ -23,14 +26,13 @@ public class CartController {
      * @apiNote create api to addItem to cart
      * @return
      */
-
     @PostMapping("/{userId}")
-    public ResponseEntity<CartDto> addItemToCart(@PathVariable String userId,
-                                                 @RequestBody AddItemsToCartRequest request) {
+    public ResponseEntity<CartDto> addItemToCart(@PathVariable String userId, @RequestBody AddItemsToCartRequest request) {
+        logger.info("Entering the request to add Iteam into cart with userId:{}",userId);
         CartDto cartDto = cartService.addItemToCart(userId, request);
+        logger.info("completed the request to add Iteam into cart with userId:{}",userId);
         return new ResponseEntity<>(cartDto, HttpStatus.CREATED);
     }
-
     /**
      * @auther Bhagyshri
      * @param userId
@@ -38,39 +40,39 @@ public class CartController {
      * @apiNote create api to removeItem from cart
      * @return
      */
-
     @DeleteMapping("/{userId}/item/{cartItemId}")
-    public ResponseEntity<ApiResponse> removeItemFromCart(@PathVariable String userId,
-                                                          @PathVariable Integer cartItemId) {
+    public ResponseEntity<ApiResponse> removeItemFromCart(@PathVariable String userId, @PathVariable Integer cartItemId) {
+        logger.info("Entering the request to remove Iteam into cart with userId:{}",userId);
         cartService.removeItemFromCart(userId, cartItemId);
         ApiResponse apiResponse = ApiResponse.builder().message("cartItem is removed successfully").success(true).status(HttpStatus.OK).build();
+        logger.info("completed the request to remove Iteam into cart with userId:{}",userId);
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
-
     /**
      * @auther Bhagyashri
      * @param userId
      * @apiNote  create api to clear all cart
      * @return
      */
-
     @DeleteMapping("/{userId}")
     public ResponseEntity<ApiResponse> clearCart(@PathVariable String userId) {
+        logger.info("Entering the request to clear cart with userId:{}",userId);
         cartService.clearCart(userId);
         ApiResponse apiResponse = ApiResponse.builder().message("cart is empty now").success(true).status(HttpStatus.OK).build();
+        logger.info("completed the request to clear cart with userId:{}",userId);
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
-
     /**
      * @auther Bhagyashri
      * @param userId
      * @apiNote create api to retrived cart data by userId
      * @return
      */
-
     @GetMapping("/{userId}")
     public ResponseEntity<CartDto> getCartByUser(@PathVariable String userId) {
+        logger.info("Entering the request to retrived cart with userId:{}",userId);
         CartDto cartDto = cartService.getCartByUser(userId);
+        logger.info("completed the request to retrived cart with userId:{}",userId);
         return new ResponseEntity<>(cartDto, HttpStatus.OK);
     }
 
